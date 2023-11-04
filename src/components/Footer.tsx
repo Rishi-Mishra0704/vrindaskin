dd2d084e94c975f016ef6937f9060bb5511c2cb2
Rishi-Mishra0704 <rishimishra0404@gmail.com>
2023-11-04 20:38:37 +0530

    Add footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,11 +1,17 @@
 import React from "react";
+import {
+  BiLogoFacebookSquare,
+  BiLogoInstagramAlt,
+  BiLogoYoutube,
+  BiLogoLinkedinSquare
+} from "react-icons/bi";
 
 const Footer = () => {
   return (
     <footer className="py-6 px-6 bg-amber-100">
       <div className="md:flex justify-between text-black">
         <div className="w-full md:w-2/3 pr-0 md:pr-10 mb-6 md:mb-0">
-          <h3 className="mb-5 font-semibold">About</h3>
+          <h3 className="mb-3 text-2xl font-semibold">About</h3>
           <p className="text-lg text-gray-800">
             Vrunda skin is built on a foundation of our ancient Ayurveda,
             science & research to provide affordable, dermatology-inspired
@@ -15,26 +21,38 @@ const Footer = () => {
           </p>
         </div>
         <div className="w-full md:w-1/3">
-          <h3 className="mb-5 font-semibold">Menu</h3>
+          <h3 className="mb-3 text-2xl font-normal">Socials</h3>
           <ul className="space-y-2">
             <li>
-              <a href="#" className="text-lg text-pink-600 hover:text-pink-700">
-                Instagram
+              <a
+                href="https://www.instagram.com/vrundaskin/?utm_medium=copy_link&hl=en"
+                className="flex text-lg text-pink-600 hover:text-pink-700"
+              >
+                <BiLogoInstagramAlt className="text-2xl mx-1" /> Instagram
               </a>
             </li>
             <li>
-              <a href="#" className="text-lg text-pink-600 hover:text-pink-700">
-                Facebook
+              <a
+                href="https://www.facebook.com/people/Vrunda-skin-oil-serum/100076303071789/?mibextid=LQQJ4d"
+                className="flex text-lg text-pink-600 hover:text-pink-700"
+              >
+                <BiLogoFacebookSquare className="text-2xl mx-1" /> Facebook
               </a>
             </li>
             <li>
-              <a href="#" className="text-lg text-pink-600 hover:text-pink-700">
-                Youtube
+              <a
+                href="https://www.youtube.com/@vrundaskin"
+                className="flex text-lg text-pink-600 hover:text-pink-700"
+              >
+                <BiLogoYoutube className='text-2xl mx-1'/> Youtube
               </a>
             </li>
             <li>
-              <a href="#" className="text-lg  text-pink-600 hover:text-pink-700">
-                LinkedIn
+              <a
+                href="https://www.linkedin.com/in/vrunda-skin-5b2720273/?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=ios_app"
+                className="flex text-lg  text-pink-600 hover:text-pink-700"
+              >
+                <BiLogoLinkedinSquare className='text-2xl mx-1'/> LinkedIn
               </a>
             </li>
           </ul>

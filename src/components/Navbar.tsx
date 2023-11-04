100bcb72ef50b7dedfda059433f34ec38498fd53
Rishi-Mishra0704 <rishimishra0404@gmail.com>
2023-11-04 22:24:50 +0530

    update navbar and footer

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,11 +1,120 @@
-import React from 'react'
+"use client";
+import React, { useState } from "react";
+import Link from "next/link";
 
 const Navbar = () => {
-  return (
-    <div>
-      Navbar
-    </div>
-  )
-}
+  const [menuOpen, setMenuOpen] = useState(false);
 
-export default Navbar
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  };
+
+  return (
+    <nav
+      className={`fixed top-0 w-full bg-[#4d79ff] z-50 ${
+        menuOpen ? "h-screen" : "h-auto"
+      }`}
+    >
+      <div className="max-w-6xl h-16 mx-auto flex justify-between items-center">
+        <Link href="/" className="text-white text-2xl font-semibold ml-4">
+          Logo
+        </Link>
+        <div
+          className={`hidden md:flex space-x-4 ${
+            menuOpen ? "hidden" : ""
+          } transition-opacity duration-300 ease-in-out`}
+        >
+          <Link href="/" className="text-white text-lg hover:underline">
+            Home
+          </Link>
+          <Link href="/#about" className="text-white text-lg hover:underline">
+            About
+          </Link>
+          <Link href="/Cars" className="text-white text-lg hover:underline">
+            Cars
+          </Link>
+          <Link href="/Rentals" className="text-white text-lg hover:underline">
+            Rentals
+          </Link>
+
+          <Link href="/#contact" className="text-white text-lg hover:underline">
+            Contact
+          </Link>
+        </div>
+        <div className="md:hidden mr-4">
+          <button className="text-white" onClick={toggleMenu}>
+            <svg
+              className={`w-6 h-6 ${
+                menuOpen ? "hidden" : ""
+              } transition-opacity duration-300 ease-in-out`}
+              fill="none"
+              stroke="currentColor"
+              viewBox="0 0 24 24"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth={2}
+                d="M4 6h16M4 12h16M4 18h16"
+              />
+            </svg>
+            <svg
+              className={`w-6 h-6 ${
+                menuOpen ? "" : "hidden"
+              } transition-opacity duration-300 ease-in-out`}
+              fill="none"
+              stroke="currentColor"
+              viewBox="0 0 24 24"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth={2}
+                d="M6 18L18 6M6 6l12 12"
+              />
+            </svg>
+          </button>
+        </div>
+      </div>
+      {menuOpen && (
+        <div className="flex flex-col justify-center my-16 items-center md:hidden bg-[#4d79ff] space-y-4 transition-opacity duration-1000 ease-in-out">
+          <Link
+            href="/"
+            className="block text-white p-2 text-3xl hover:bg-[#3c64e1]"
+          >
+            Home
+          </Link>
+          <Link
+            href="/#about"
+            className="block text-white p-2 text-3xl  hover:bg-[#3c64e1]"
+          >
+            About
+          </Link>
+          <Link
+            href="/Cars"
+            className="block text-white p-2 text-3xl hover:bg-[#3c64e1]"
+          >
+            Cars
+          </Link>
+          <Link
+            href="/Rentals"
+            className="block text-white text-3xl hover:underline"
+          >
+            Rentals
+          </Link>
+
+          <Link
+            href="/#contact"
+            className="block text-white p-2 text-3xl  hover:bg-[#3c64e1]"
+          >
+            Contact
+          </Link>
+        </div>
+      )}
+    </nav>
+  );
+};
+
+export default Navbar;

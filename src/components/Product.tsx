48e90d9d812e3131f5b27edab4479d03d371280d
Rishi Mishra <114818980+Rishi-Mishra0704@users.noreply.github.com>
2023-11-11 02:12:46 +0530

    Update Product.tsx

--- src/components/Product.tsx
+++ src/components/Product.tsx
@@ -1,14 +1,12 @@
 import Image from 'next/image';
 import React from 'react';
-import prod1 from '@/assets/prod1.png';
-import prod2 from '@/assets/prod2.png';
 
 const Product = () => {
   return (
     <div className="flex flex-col sm:flex-row bg-white rounded-lg overflow-hidden shadow-lg mx-auto mt-10 max-w-md p-4">
       <div className="sm:w-1/2 mb-4 sm:mb-0">
         <Image
-          src={prod1} // Dummy image URL
+          src='../assets/prod2.png'
           alt="Product"
           objectFit="cover"
           width={300}
@@ -26,7 +24,7 @@ const Product = () => {
       </div>
       <div className="sm:w-1/2">
         <Image
-          src={prod2} // Dummy image URL
+          src='../assets/prod2.png'
           alt="Product"
           objectFit="cover"
           width={300}
